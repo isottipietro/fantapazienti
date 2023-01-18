8c7a58176362dc02861b61139e4cb5e3cab6a6e9
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-18 18:38:32 +0100

    1

--- login.php
+++ login.php
@@ -6,6 +6,9 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
 </head>
 <body>
+<div class="box">
+<p><h1>prova</h1> ciao ciao</p>
+</div>
   <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900" preserveAspectRatio="xMidYMax slice">
 		<defs>
@@ -56,7 +59,5 @@
 			</use>
 		</g>
 	</svg>
-    <div class=box>
-        prova
-</div>
+    
 </body>
