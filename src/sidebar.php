bd27f672a6572c90083f34a09dd796d97b941434
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 16:08:49 +0100

    a

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,3 +1,6 @@
 <div id="sidebar">
     <p>prova</p>
+    <p> <?php printf("Benvenuto in FantaRia %s, ricordati la prima regola della FantaRia: non si parla della FantaRia", $session_username);
+	    echo "<br>";
+	    printf("%s", '<a href="src/logout.php">logout</a>');?></p>
 </div>
