15deb4e85f78bc92538533b9e603da92aba2af41
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 01:57:42 +0100

    1

---
+++ src/header.php
@@ -0,0 +1,5 @@
+<div id="header">
+  <img src="assets/images/logo.png" style="float: left; margin-right: 10px;">
+  <h1>Fantapazienti</h1>
+  <h3><?php echo $saggezza[array_rand($saggezza)]; ?></h3>
+</div>
