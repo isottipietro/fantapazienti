4146772e6d7c3fedd238e43462d5829e3baa79b2
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 18:30:15 +0100

    1

--- src/sidebar.php
+++ src/sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar">
-    <img src="/assets/images/user.png" style="width: 100px; align: center;">
+    <img src="/assets/images/user.png" style="width: 50%;margin: auto;display: block;padding-top: 50px;">
 <p style="text-align: center;">profilo</p>
 <hr color="#151920" />
     <p>dashboard</p>
