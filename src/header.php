1c0d86a70ae846712c9ce97f25902b08af5a6998
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 17:21:33 +0100

    1

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <div id="header">
   <div class="header-button">1</div>
   <div class="header-button">2</div>
-  <div class="header-button">3</div>
+  <div class="header-button"><a href="src/logout.php">esci</a></div>
 </div>
