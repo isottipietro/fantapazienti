94a4cd25a597a350644bb071bd67e0371340709e
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 18:33:08 +0100

    1

--- src/sidebar.php
+++ src/sidebar.php
@@ -2,8 +2,8 @@
     <img src="/assets/images/user.png" style="width: 50%;margin: auto;display: block;padding-top: 50px;">
 <p style="text-align: center;">profilo</p>
 <hr color="#151920" />
-    <p>dashboard</p>
-    <p>+ eventi</p>
+    <p><a href="dashboard.php">dashboard</a></p>
+    <p><a href="eventi.php">eventi</a></p>
     <p>+ pazienti</p>
     <p>asta</p>
     <p>invita</p>
