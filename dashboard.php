ec5b51d8fc7ae5c8c52d7d198876863c72ed7601
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 01:55:15 +0100

    1

--- dashboard.php
+++ dashboard.php
@@ -19,14 +19,14 @@
 	if (isset($_SESSION['session_id'])) {
     	$session_user = htmlspecialchars($_SESSION['session_user'], ENT_QUOTES, 'UTF-8');
     	$session_id = htmlspecialchars($_SESSION['session_id']);
-	
-    printf("Benvenuto %s, il tuo session ID è %s", $session_user, $session_id);
-    echo "<br>";
-    printf("%s", '<a href="src/logout.php">logout</a>');
-} else {
-    printf("Effettua il %s per accedere all'area riservata", '<a href="../login.html">login</a>');
-}
-	?>	<div id="wrapper">
+	    printf("Benvenuto %s", $session_user);
+	    echo "<br>";
+	    printf("%s", '<a href="src/logout.php">logout</a>');
+	} else {
+		$msg = urlencode("Devi effettuare il login per poter accedere al fantapazienti");
+		header("Location: /welcome.php?Message=".$msg);
+	}?>
+	<div id="wrapper">
 		<?php include 'src/header.php'; ?>
 	<div id="nav">
 		<button class="tablink" onclick="openPage('Singola', this, 'green')" id="defaultOpen">Etichetta Singola</button>
