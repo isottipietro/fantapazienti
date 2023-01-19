aa7f77cd589a140c36778800c316789ede715015
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 01:05:35 +0100

    1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
-	
+
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
 	<script src="src/menubar.js" defer></script>
@@ -20,6 +20,12 @@
     	$session_user = htmlspecialchars($_SESSION['session_user'], ENT_QUOTES, 'UTF-8');
     	$session_id = htmlspecialchars($_SESSION['session_id']);
 	
+    printf("Benvenuto %s, il tuo session ID è %s", $session_user, $session_id);
+    echo "<br>";
+    printf("%s", '<a href="logout.php">logout</a>');
+} else {
+    printf("Effettua il %s per accedere all'area riservata", '<a href="../login.html">login</a>');
+}
 	?>	<div id="wrapper">
 		<?php include 'src/header.php'; ?>
 	<div id="nav">
