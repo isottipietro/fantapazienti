da950e2e594655dc80c7d390bb654f1bc97c5c7f
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-19 18:05:59 +0100

    1

--- src/header.php
+++ src/header.php
@@ -2,5 +2,5 @@
   <div class="header-button"><a href="src/logout.php">ESCI</a></div>
   <div class="header-button">PAZIENTI</div>
   <div class="header-button">EVENTI</div>
-  <img src="/assets/images/logo.png" style="height: 60px;">
+  <img src="/assets/images/logo.png" style="height: 60px; float: right; padding: 0 10px;">
 </div>
