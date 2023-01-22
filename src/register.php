cbeb00ea7e7a0ef541518f909b0ecbadc4b9b7c3
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-22 19:51:14 +0100

    1

--- src/register.php
+++ src/register.php
@@ -4,27 +4,7 @@ require_once('database.php');
 if (isset($_POST['register'])) {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
-    $isUsernameValid = filter_var(
-        $username,
-        FILTER_VALIDATE_REGEXP, [
-            "options" => [
-                "regexp" => "/^[a-z\d_]{3,20}$/i"
-            ]
-        ]
-    );
-    $pwdLenght = mb_strlen($password);
-    
-    if (empty($username) || empty($password)) {
-        $msg = 'Compila tutti i campi %s';
-    } elseif (false === $isUsernameValid) {
-        $msg = 'Lo username non è valido. Sono ammessi solamente caratteri 
-                alfanumerici e l\'underscore. Lunghezza minina 3 caratteri.
-                Lunghezza massima 20 caratteri';
-    } elseif ($pwdLenght < 8 || $pwdLenght > 20) {
-        $msg = 'Lunghezza minima password 8 caratteri.
-                Lunghezza massima 20 caratteri';
-    } else {
-        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
         $query = "
             SELECT ID
@@ -57,8 +37,6 @@ if (isset($_POST['register'])) {
                 $msg = 'Problemi con l\'inserimento dei dati %s';
             }
         }
-    }
     
     printf($msg, '<a href="../register.html">torna indietro</a>');
 }
-?>
