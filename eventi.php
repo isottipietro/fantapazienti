31829251fd115ace804db50a554573020d4b9d7b
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-22 18:11:47 +0100

    1

---
+++ eventi.php
@@ -0,0 +1,10 @@
+<?php include 'src/head.php';?>
+<?php $pageName = "Eventi";?>
+	<?php include 'src/sidebar.php'; ?>
+	<?php include 'src/header.php'; ?>
+	<div id="page">
+</div>
+  <?php include 'src/footer.php';?>
+</div>
+</body>
+</html>
