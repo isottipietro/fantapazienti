03391b700e831710b7342fd2eeec266107810ec3
isottipietro <86808453+isottipietro@users.noreply.github.com>
2023-01-22 19:24:46 +0100

    1

--- invita.php
+++ invita.php
@@ -3,6 +3,13 @@
 <?php include 'src/sidebar.php'; ?>
 <?php include 'src/header.php'; ?>
 <div id="page">
+  <form method="post" action="/src/register.php">
+    <h1>Registrazione</h1>
+    <input type="text" id="username" placeholder="Username" name="username" maxlength="50" required>
+    <input type="password" id="password" placeholder="Password" name="password" required>
+    <button type="submit" name="register">Registrati</button>
+  </form>
+
   <?php include 'src/footer.php';?>
 </div>
 </body>
